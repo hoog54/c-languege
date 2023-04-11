17cedbfedfa5f318b80ae4f87a99c20f447da928
장홍규 <a31397720@gmail.com>
2023-04-11 06:37:11 +0000

    dsfsdf

---
+++ 47.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	
+	int num1 = 14;//변수선언 및 초기화
+	int unm2 = 14;
+	int unm3 = 0;//포기화
+	
+    sun = num1 + num2;
+	pritnf("%d + %d = \n",sum1,num2,sum);
+		
+   return 0;	
+}
