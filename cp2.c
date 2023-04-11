ce15222bc45f6a02e5345009798e6b67f4d1763a
장홍규 <a31397720@gmail.com>
2023-04-11 07:28:42 +0000

    uuuuu

---
+++ cp2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main(){
+	 int unm = 0;
+	 pritnf("정수를 입력 : ");
+	 scanf("%d," &unm);
+	 
+	 if ( (num%2) == 0) {
+		 printf("짝수입니다.\n");
+	 }else {
+		 printf("홀수입니다.\n");
+	 }
+	 return 0 ;
+ }
