7daf074b4bc0afebaa9232d82f54929bf033aa44
장홍규 <a31397720@gmail.com>
2023-04-11 06:58:47 +0000

    dfsafs

---
+++ 53.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+	float weight = 0, height = 0, bmi = 0;
+	printf("몸무게를 입력(kg) : ");
+	scanf("%f", &weight);
+	printf("신자을 입력(cm) : ");
+	scanf("%f", &height);
+	height = height * 0.01;
+	bmi = weight / (height * height);
+	printf("당신의 BMI는 %.2f\n", bmi);
+	return 0;
+}	
+	
+	
---
+++ 55.c
@@ -0,0 +1,14 @@
+#include() <stdio.h>
+
+int main() {
+	
+   int a = 3;
+   a = a + 6;
+	
+	if(a>5) {
+		printf("%d\n", a);
+	    printf("조건이 참일을 때, 실행 \n");
+	}else {
+		printf("%d\n", a);
+	    printf("조건이 거짓을을 때, 실행 \n");
+}	
